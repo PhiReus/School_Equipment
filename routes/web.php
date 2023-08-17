3a2807be4e1dd0f677665e2bc806690459b95a79
PhiReus <127011502+PhiReus@users.noreply.github.com>
2023-08-17 15:07:43 +0700

    Toi uu Route

--- routes/web.php
+++ routes/web.php
@@ -18,30 +18,25 @@ use Illuminate\Support\Facades\Route;
 // Route::get('/', function () {
 //     return view('layouts/master');
 // });
+
+//Group
 Route::prefix('groups')->group(function () {
-    Route::get('/', [GroupController::class, 'index'])->name('groups.index');
-    Route::get('/create', [GroupController::class, 'create'])->name('groups.create');
-    Route::post('/store', [GroupController::class, 'store'])->name('groups.store');
-    Route::get('/edit/{id}', [GroupController::class, 'edit'])->name('groups.edit');
-    Route::put('/update/{id}', [GroupController::class, 'update'])->name('groups.update');
-    Route::delete('/destroy/{id}', [GroupController::class, 'destroy'])->name('groups.destroy');
-    Route::get('/search', [GroupController::class, 'search'])->name('groups.search');
     Route::get('/show/{id}', [GroupController::class, 'show'])->name('groups.show');
     Route::put('/saveRoles/{id}', [GroupController::class, 'saveRoles'])->name('groups.saveRoles');
 });
+Route::resource('groups',\App\Http\Controllers\GroupController::class);
+
+
+//User
 Route::prefix('users')->group(function () {
-    Route::get('/', [UserController::class, 'index'])->name('users.index');
-    Route::get('/create', [UserController::class, 'create'])->name('users.create');
-    Route::post('/store', [UserController::class, 'store'])->name('users.store');
-    Route::get('/edit/{id}', [UserController::class, 'edit'])->name('users.edit');
-    Route::put('/update/{id}', [UserController::class, 'update'])->name('users.update');
-    Route::delete('/destroy/{id}', [UserController::class, 'destroy'])->name('users.destroy');
-    Route::get('/show{id}', [UserController::class, 'show'])->name('users.show');
     Route::get('/export', [UserController::class, 'export'])->name('users.export');
     Route::get('/trash', [UserController::class, 'trash'])->name('users.trash');
     Route::get('/restore/{id}', [UserController::class, 'restore'])->name('users.restore');
     Route::delete('/force_destroy/{id}', [UserController::class, 'force_destroy'])->name('users.force_destroy');
 });
+Route::resource('users',\App\Http\Controllers\UserController::class);
+
+
 //Device
 Route::prefix('devices')->group(function () {
     Route::get('/trash', [\App\Http\Controllers\DeviceController::class, 'trash'])->name('devices.trash');
@@ -58,5 +53,3 @@ Route::prefix('rooms')->group(function () {
 });
 Route::resource('rooms',\App\Http\Controllers\RoomController::class);
 
-// Group
-Route::resource('groups',\App\Http\Controllers\GroupController::class);
